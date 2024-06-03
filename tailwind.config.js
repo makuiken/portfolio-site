371bda1d53a5f2bcbc7524474dafe0d573863e96
makuiken <mkuiken18@outlook.com>
2024-06-02 23:02:55 -0400

    Add extra styling, new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     fontFamily: {
-      display: ["Lexend"],
+      display: ["Major Mono Display"],
     },
   },
   plugins: [],
 };
+  
